c2eb425112eab8351bcffd5b6df967cd3c99ebda
littlepawpaw <bjzhangrunbo19@126.com>
2025-01-11 18:13:32 +0000

    Update PyCrystal.py

--- PyCrystal.py
+++ PyCrystal.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pyvista as pv
 
+
 class UnitCell:
     def __init__(self,
                  a=1.0, b=1.0, c=1.0,
@@ -35,20 +36,68 @@ class UnitCell:
     def add_lattice(self, points: np.ndarray):
         self.lattice = np.append(self.lattice, points, axis=0)
 
-
     def cartesian(self):
         return np.matmul(self.matrix, self.lattice.transpose()).transpose()
 
+    def reciprocal(self):
+        return 2 * np.pi * np.linalg.inv(self.primitive).T
+
+    def plot(self, r=0.05, primitive=False):
+        # Create a PyVista plotter
+        plotter = pv.Plotter()
+        lattice_points = self.cartesian()
+
+        # Plot the lattice points as spheres
+        for point in self.cartesian():
+            sphere = pv.Sphere(radius=r, center=point)
+            plotter.add_mesh(sphere, color='blue', smooth_shading=True)
+
+        # Add the edges to represent the cell boundaries
+        cell_edges = self.edge
+        lines = []
+        for edge in cell_edges:
+            lines.append([lattice_points[edge[0]], lattice_points[edge[1]]])
+
+        # Convert to PolyData format for lines
+        lines = np.array(lines)
+        plotter.add_lines(lines.reshape(-1, 3), color="black")
+
+        origin = np.array([0, 0, 0])
+
+        if primitive:
+            # Add the vectors
+            for vec in self.primitive.T:
+                plotter.add_arrows(cent=origin, direction=vec, color="red", smooth_shading=True)
+
+        plotter.show()
+
+
+"""
+Cubic systems
+"""
+
+
+def cubic(group="P", **kwargs):
+    if group == "P":
+        return SC(**kwargs)
+    elif group == "I":
+        return BCC(**kwargs)
+    elif group == "F":
+        return FCC(**kwargs)
+    else:
+        raise ValueError("Wrong point group!")
+
 
 class SC(UnitCell):
-    def __init__(self, a=1.0):
-        super().__init__(a=a, b=a, c=a)
+    def __init__(self, a=1.0, **kwargs):
+        super().__init__(a=a, b=a, c=a, **kwargs)
 
         self.primitive = self.matrix
 
+
 class BCC(UnitCell):
-    def __init__(self, a=1.0):
-        super().__init__(a=a, b=a, c=a)
+    def __init__(self, a=1.0, **kwargs):
+        super().__init__(a=a, b=a, c=a, **kwargs)
 
         self.add_lattice(np.array([[0.5, 0.5, 0.5]]))
 
@@ -57,11 +106,12 @@ class BCC(UnitCell):
             [0.5, -0.5, 0.5],
             [-0.5, 0.5, 0.5]
         ])
-        self.primitive = np.matmul(bravais, self.matrix)
+        self.primitive = np.matmul(self.matrix, bravais)
+
 
 class FCC(UnitCell):
-    def __init__(self, a=1.0):
-        super().__init__(a=a, b=a, c=a)
+    def __init__(self, a=1.0, **kwargs):
+        super().__init__(a=a, b=a, c=a, **kwargs)
 
         self.add_lattice(np.array([[0, 0.5, 0.5],
                                    [0.5, 0, 0.5],
@@ -75,53 +125,103 @@ class FCC(UnitCell):
             [0.5, 0, 0.5],
             [0.5, 0.5, 0]
         ])
-        self.primitive = np.matmul(bravais, self.matrix)
+        self.primitive = np.matmul(self.matrix, bravais)
 
-class ST(UnitCell):
-    def __init__(self, a=1.0, c=2):
-        super().__init__(a=a, b=a, c=c)
+
+"""
+Tetragonal systems
+"""
+
+
+def tetragonal(group="P", **kwargs):
+    if group == "P":
+        return STetr(**kwargs)
+    elif group == "I":
+        return BCT(**kwargs)
+    else:
+        raise ValueError("Wrong point group!")
+
+
+class STetr(UnitCell):
+    def __init__(self, a=1.0, c=2, **kwargs):
+        super().__init__(a=a, b=a, c=c, **kwargs)
 
         self.primitive_vectors = self.matrix
 
 
 class BCT(UnitCell):
-    def __init__(self, a=1.0, c=2):
-        super().__init__(a=a, b=a, c=c)
+    def __init__(self, a=1.0, c=2, **kwargs):
+        super().__init__(a=a, b=a, c=c, **kwargs)
 
         self.add_lattice(np.array([[0.5, 0.5, 0.5]]))
 
         bravais = np.array([
-            [0.5, 0.5, 0.5],
-            [0.5, -0.5, -0.5],
-            [-0.5, 0.5, -0.5]
+            [-0.5, 0.5, 0.5],
+            [0.5, -0.5, 0.5],
+            [0.5, 0.5, -0.5]
         ])
-        self.primitive = np.matmul(bravais, self.matrix)
+        self.primitive = np.matmul(self.matrix, bravais)
+
+
+"""
+Orthorhombic systems
+"""
+
+
+def orthorhombic(group="P", **kwargs):
+    if group == "P":
+        return SO(**kwargs)
+    elif group == "I":
+        return BCO(**kwargs)
+    elif group == "C":
+        return ECO(**kwargs)
+    elif group == "F":
+        return FCO(**kwargs)
+    else:
+        raise ValueError("Wrong point group!")
 
 
 class SO(UnitCell):
-    def __init__(self, a=1.0, b=2, c=3):
-        super().__init__(a=a, b=b, c=c)
+    def __init__(self, a=1.0, b=2, c=3, **kwargs):
+        super().__init__(a=a, b=b, c=c, **kwargs)
 
         self.primitive_vectors = self.matrix
 
 
 class BCO(UnitCell):
-    def __init__(self, a=1.0, b=2, c=3):
-        super().__init__(a=a, b=b, c=c)
+    def __init__(self, a=1.0, b=2, c=3, **kwargs):
+        super().__init__(a=a, b=b, c=c, **kwargs)
 
         self.add_lattice(np.array([[0.5, 0.5, 0.5]]))
 
         bravais = np.array([
+            [-0.5, 0.5, 0.5],
+            [0.5, -0.5, 0.5],
+            [0.5, 0.5, -0.5]
+        ])
+        self.primitive = np.matmul(self.matrix, bravais)
+
+
+class ECO(UnitCell):
+    def __init__(self, a=1.0, b=2, c=3, **kwargs):
+        super().__init__(a=a, b=b, c=c, **kwargs)
+
+        self.add_lattice(np.array([
             [0.5, 0.5, 0],
-            [1, 0, 0],
+            [0.5, 0.5, 1]
+        ]))
+
+        bravais = np.array([
+            [0.5, 0.5, 0],
+            [-0.5, 0.5, 0],
             [0, 0, 1]
         ])
-        self.primitive = np.matmul(bravais, self.matrix)
+        self.primitive = np.matmul(self.matrix, bravais)
 
 
 class FCO(UnitCell):
-    def __init__(self, a=1.0, b=2, c=3):
-        super().__init__(a=a, b=b, c=c)
+    def __init__(self, a=1.0, b=2, c=3, **kwargs):
+        super().__init__(a=a, b=b, c=c, **kwargs)
 
         self.add_lattice(np.array([[0, 0.5, 0.5],
                                    [0.5, 0, 0.5],
@@ -131,21 +231,128 @@ class FCO(UnitCell):
                                    [0.5, 0.5, 1]]))
 
         bravais = np.array([
-            [0.5, 0.5, 0],
             [0, 0.5, 0.5],
-            [0.5, 0, 0.5]
+            [0.5, 0, 0.5],
+            [0.5, 0.5, 0]
         ])
-        self.primitive = np.matmul(bravais, self.matrix)
+        self.primitive = np.matmul(self.matrix, bravais)
 
 
-class SH(UnitCell):
-    def __init__(self, a, c):
-        super().__init__(a=a, b=a, c=c, gamma=120)
+"""
+Monoclinic systems
+"""
+
+
+def monoclinic(group="P", **kwargs):
+    if group == "P":
+        return SM(**kwargs)
+    elif group == "C":
+        return ECM(**kwargs)
+    else:
+        raise ValueError("Wrong point group!")
+
+
+class SM(UnitCell):
+    def __init__(self, a=1.0, b=2, c=3, beta=120, **kwargs):
+        super().__init__(a=a, b=b, c=c, beta=beta, **kwargs)
 
         bravais = np.array([
             [1, 0, 0],
-            [-0.5, np.sqrt(3) / 2, 0],
+            [0, 1, 0],
             [0, 0, 1]
         ])
+        self.primitive = np.matmul(self.matrix, bravais)
 
-        self.primitive = np.matmul(bravais, self.matrix)
+
+class ECM(UnitCell):
+    def __init__(self, a=1.0, b=2, c=3, beta=120, **kwargs):
+        super().__init__(a=a, b=b, c=c, beta=beta, **kwargs)
+
+        self.add_lattice(np.array([
+            [0.5, 0.5, 0],
+            [0.5, 0.5, 1]
+        ]))
+
+        bravais = np.array([
+            [0.5, 0.5, 0],
+            [-0.5, 0.5, 0],
+            [0, 0, 1]
+        ])
+        self.primitive = np.matmul(self.matrix, bravais)
+
+
+"""
+Triclinic systems
+"""
+
+
+def triclinic(group="P", **kwargs):
+    if group == "P":
+        return STric(**kwargs)
+    else:
+        raise ValueError("Wrong point group!")
+
+
+class STric(UnitCell):
+    def __init__(self, a=1.0, b=2, c=3, alpha=60, beta=70, gamma=80, **kwargs):
+        super().__init__(a=a, b=b, c=c, alpha=alpha, beta=beta, gamma=gamma, **kwargs)
+
+        bravais = np.array([
+            [1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1]
+        ])
+        self.primitive = np.matmul(self.matrix, bravais)
+
+
+"""
+Trigonal systems
+"""
+
+
+def trigonal(group="P", **kwargs):
+    if group == "P":
+        return STrig(**kwargs)
+    else:
+        raise ValueError("Wrong point group!")
+
+
+class STrig(UnitCell):
+    def __init__(self, a=1.0, c=2, gamma=120, **kwargs):
+        super().__init__(a=a, b=a, c=c, gamma=gamma, **kwargs)
+
+        bravais = np.array([
+            [1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1]
+        ])
+        self.primitive = np.matmul(self.matrix, bravais)
+
+
+"""
+Hexagonal systems
+"""
+
+
+def hexagonal(group="P", **kwargs):
+    if group == "P":
+        return SH(**kwargs)
+    else:
+        raise ValueError("Wrong point group!")
+
+
+class SH(UnitCell):
+    def __init__(self, a=1.0, c=2, gamma=120, **kwargs):
+        super().__init__(a=a, b=a, c=c, gamma=gamma, **kwargs)
+
+        bravais = np.array([
+            [1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1]
+        ])
+        self.primitive = np.matmul(self.matrix, bravais)
+
+
+c1 = cubic("I")
+c1.plot(primitive=True)
+print(c1.reciprocal())
